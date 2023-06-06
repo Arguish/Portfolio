32e7b4149cb15364d0eb191c62c8721347d2702b
Arguish <jhergon8@gmail.com>
2023-06-06 01:46:20 +0100

    init

---
+++ src/content_option.js
@@ -0,0 +1,135 @@
+import MF from "./assets/MF.png";
+import ZB from "./assets/ZB.png";
+import JC from "./assets/JC.png";
+
+const logotext = "Javier Hernandez";
+const meta = {
+  title: "JAV-STACK",
+  description:
+    "I’m Javier Hernandez Full stack devloper, ready to work with you",
+};
+
+const introdata = {
+  title: "While (true):",
+  animated: {
+    first: "LEARN",
+    second: "CODE",
+    third: "ENJOY",
+  },
+  description:
+    "Mi trayectoria profesional ha sido diversa y enriquecedora. Ahora, mi compromiso es dedicar todo mi tiempo y energía a perfeccionar mi oficio como programador, impulsado por el deseo de seguir creciendo y materializando proyectos sorprendentes",
+  your_img_url:
+    "./assets/photo_5999151787694733785_w_topaz_denoise_enhance_faceai_sharpen.jpeg",
+};
+
+const dataabout = {
+  title: "abit about my self",
+  aboutme:
+    "Mi trayectoria profesional ha sido diversa y enriquecedora. Comencé mi carrera como Jefe de Cocina, donde aprendí a manejar el arte de gestionar equipos. Posteriormente, exploré el mundo del sonido como técnico en una radio local, afinando mi oído para crear experiencias auditivas cautivadoras. Luego, tuve el privilegio de trabajar como técnico de luces para reconocidos artistas como Peta Zeta, Julieta Venegas y más tarde como Operador de Cámara para Mclan, Estopa y los Mojinos. Sin embargo, fue a través de mi formación en Reboot Academy donde descubrí mi verdadera pasión: la programación. En este campo fascinante, encontré la perfecta amalgama entre creatividad y lógica, donde cada línea de código se convierte en una herramienta poderosa para dar vida a soluciones innovadoras. Ahora, mi compromiso es dedicar todo mi tiempo y energía a perfeccionar mi oficio como programador, impulsado por el deseo de seguir creciendo y materializando proyectos sorprendentes",
+};
+const worktimeline = [
+  {
+    jobtitle: "Designer of week",
+    where: "YAdfi",
+    date: "2020",
+  },
+  {
+    jobtitle: "Designer of week",
+    where: "Jamalya",
+    date: "2019",
+  },
+  {
+    jobtitle: "Designer of week",
+    where: "ALquds",
+    date: "2019",
+  },
+];
+
+const skills = [
+  {
+    name: "Python",
+    value: 90,
+  },
+  {
+    name: "Djano",
+    value: 85,
+  },
+  {
+    name: "Javascript",
+    value: 80,
+  },
+  {
+    name: "React",
+    value: 60,
+  },
+  {
+    name: "Jquery",
+    value: 85,
+  },
+];
+
+const services = [
+  {
+    title: "UI & UX Design",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at nisl euismod urna bibendum sollicitudin.",
+  },
+  {
+    title: "Mobile Apps",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at nisl euismod urna bibendum sollicitudin.",
+  },
+  {
+    title: "Wordpress Design",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at nisl euismod urna bibendum sollicitudin.",
+  },
+];
+
+const dataportfolio = [
+  {
+    img: MF,
+    description: "MediaFast",
+    link: "https://mediafast.netlify.app",
+  },
+  {
+    img: ZB,
+    description: "Zombie Game",
+    link: "https://arguish.github.io/Zombi0.1/",
+  },
+  {
+    img: JC,
+    description: "Componentes Experimentales",
+    link: "https://javcomponents.netlify.app/",
+  },
+];
+
+const contactConfig = {
+  YOUR_EMAIL: "jhergon8@gmail.com",
+  YOUR_FONE: "+34 634 466 788",
+  description: "",
+  // creat an emailjs.com account
+  // check out this tutorial https://www.emailjs.com/docs/examples/reactjs/
+  YOUR_SERVICE_ID: "service_id",
+  YOUR_TEMPLATE_ID: "template_id",
+  YOUR_USER_ID: "user_id",
+};
+
+const socialprofils = {
+  github: "https://github.com/Arguish",
+  facebook: "https://facebook.com",
+  linkedin: "https://linkedin.com/in/javhergondev/",
+  twitter: "https://twitter.com",
+};
+export {
+  meta,
+  dataabout,
+  dataportfolio,
+  worktimeline,
+  skills,
+  services,
+  introdata,
+  contactConfig,
+  socialprofils,
+  logotext,
+};
---
+++ src/pages/home/index.js
@@ -0,0 +1,86 @@
+import React from "react";
+import "./style.css";
+import { Helmet, HelmetProvider } from "react-helmet-async";
+import Typewriter from "typewriter-effect";
+import { introdata, meta } from "../../content_option";
+import { Link } from "react-router-dom";
+import back from "../../assets/photo_5999151787694733785_w_topaz_denoise_enhance_faceai_sharpen.jpeg";
+
+export const Home = () => {
+  return (
+    <HelmetProvider>
+      <section id="home" className="home">
+        <Helmet>
+          <meta charSet="utf-8" />
+          <title> {meta.title}</title>
+          <meta name="description" content={meta.description} />
+        </Helmet>
+        <div className="intro_sec d-block d-lg-flex align-items-center ">
+          <div className="h_bg-image order-1 order-lg-2 h-100 ">
+            <div
+              style={{
+                background: `linear-gradient(0.25turn, rgba(0,0,0,1), rgba(0,0,0,0))`,
+                width: "80vw",
+                height: "100vh",
+                transform: "translateX(-700px)",
+                position: "absolute",
+                zIndex: "50",
+              }}
+            ></div>
+            <img
+              style={{
+                height: "100vh",
+                transform: "translateX(-200px)",
+              }}
+              src={back}
+            ></img>
+          </div>
+          <div
+            style={{ zIndex: "100" }}
+            className="text order-2 order-lg-1 h-100 d-lg-flex justify-content-center"
+          >
+            <div className="align-self-center ">
+              <div className="intro mx-auto">
+                <h2 className="mb-1x">Javier Herández</h2>
+                <hr />
+                <h2 className="mb-1x">{introdata.title}</h2>
+                <h1 className="fluidz-48 mb-1x">
+                  <Typewriter
+                    options={{
+                      strings: [
+                        introdata.animated.first,
+                        introdata.animated.second,
+                      ],
+                      autoStart: true,
+                      loop: true,
+                      deleteSpeed: 10,
+                    }}
+                  />
+                </h1>
+                <p className="mb-1x">{introdata.description}</p>
+                <div className="intro_btn-action pb-5">
+                  <Link to="/portfolio" className="text_2">
+                    <div id="button_p" className="ac_btn btn ">
+                      Mi Portfolio
+                      <div className="ring one"></div>
+                      <div className="ring two"></div>
+                      <div className="ring three"></div>
+                    </div>
+                  </Link>
+                  <Link to="/contact">
+                    <div id="button_h" className="ac_btn btn">
+                      Contactame!
+                      <div className="ring one"></div>
+                      <div className="ring two"></div>
+                      <div className="ring three"></div>
+                    </div>
+                  </Link>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+    </HelmetProvider>
+  );
+};
